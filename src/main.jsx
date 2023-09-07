a81b4be61dfea3d05865864e98e4e03548fbf57b
Brian Gorman <briangorman99@gmail.com>
2023-09-07 13:46:21 -0400

    add Fable, Home, and Search pages, add pages to router

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
+import { Home, Search, Fable } from "./pages";
+
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [],
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+      {
+        path: "/fables/:id",
+        element: <Fable />,
+      },
+    ],
   },
 ]);
 
---
+++ src/pages/Fable.jsx
@@ -0,0 +1,7 @@
+export default function Fable() {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+}
