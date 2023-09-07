fadd5b21f19e4dfd790c68c898da2830ae333e65
Brian Gorman <briangorman99@gmail.com>
2023-09-07 14:19:07 -0400

    add FableCard component, add grid of FableCards to Home page

--- src/components/FableCard.jsx
+++ src/components/FableCard.jsx
@@ -1,3 +1,14 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Heading,
+  Text,
+  Box,
+  Stack,
+} from "@chakra-ui/react";
+
 export default function FableCard({
   title,
   image,
@@ -6,13 +17,21 @@ export default function FableCard({
   lastUpdated,
 }) {
   return (
-    <div>
-      <img src={image} alt={title} />
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <span>
-        {author} {lastUpdated}
-      </span>
-    </div>
+    <Card maxW={"445px"} w={"full"} rounded="lg" overflow={"hidden"}>
+      <Box h={"300px"} bg={"gray.500"} pos={"relative"}>
+        <Image h="100%" w="100%" src={image} alt={title} objectFit="cover" />
+      </Box>
+
+      <CardBody>
+        <Stack>
+          <Heading>{title}</Heading>
+          <Text>{description}</Text>
+        </Stack>
+      </CardBody>
+
+      <CardFooter gap={"3"}>
+        <Text>{author}</Text> · <Text>{lastUpdated}</Text>
+      </CardFooter>
+    </Card>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,51 @@
+import { SimpleGrid } from "@chakra-ui/react";
+import FableCard from "../components/FableCard";
+
+const fableData = [
+  {
+    title: "Harry Potter",
+    image: "https://picsum.photos/300",
+    description:
+      "A boy learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own.",
+    author: "J.K. Rowling",
+    lastUpdated: "2007",
+  },
+  {
+    title: "Harry Potter",
+    image: "https://picsum.photos/300",
+    description:
+      "A boy learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own.",
+    author: "J.K. Rowling",
+    lastUpdated: "2007",
+  },
+  {
+    title: "Harry Potter",
+    image: "https://picsum.photos/300",
+    description:
+      "A boy learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own.",
+    author: "J.K. Rowling",
+    lastUpdated: "2007",
+  },
+  {
+    title: "Harry Potter",
+    image: "https://picsum.photos/300",
+    description:
+      "A boy learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own.",
+    author: "J.K. Rowling",
+    lastUpdated: "2007",
+  },
+];
+
 export default function Home() {
   return (
-    <div>
-      <div></div>
-    </div>
+    <SimpleGrid
+      p="12"
+      columns={{ base: 1, md: 2, xl: 3 }}
+      spacing={{ base: 6, lg: 8 }}
+    >
+      {fableData.map((data) => (
+        <FableCard key={data.title} {...data} />
+      ))}
+    </SimpleGrid>
   );
 }
